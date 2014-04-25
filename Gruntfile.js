08ac6de264458f828bc9635d6e3257c9b37c7675
Musterknabe <dermusterknabe@gmail.com>
2014-04-25 15:35:01 +0200

    [build] Activated variable mangling on minifying

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
 
     uglify: {
       options: {
-        mangle: false,
+        mangle: true,
         banner: '' +
           '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
           '<%= pkg.homepage ? " *  " + pkg.homepage + "\\n" : "" %>' +
